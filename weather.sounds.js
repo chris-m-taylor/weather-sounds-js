f6aa41a0b42f2738ac83b6a27516a28314e96f86
John G Thevos <admin@mindcette.com>
2020-03-02 16:34:41 -0500

    basic app framework with p5.js libraries

---
+++ weather.sounds.js
@@ -0,0 +1,10 @@
+let data; // this will serve to capture the data we need from get requests.
+
+function setup() {
+    createCanvas(600,600)
+    background(0)
+}
+
+function draw() {
+    // draw something with them here.
+}
